bf562577d60e3f2553c61bbfe86edabde97eaae0
¨jumartinezfer¨ <jumartinezfer95@gmail.com>
2025-07-29 19:11:28 -0500

    actualizando rama

--- beland-backend/src/users/users.controller.ts
+++ beland-backend/src/users/users.controller.ts
@@ -1,34 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Query, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Controladores de Usuarios')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Crear un usuario' })
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
   }
-
+  
+  @ApiOperation({ summary: 'Obtener todos los usuarios' })
   @Get()
   async findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
     return await this.usersService.findAll(page, limit);
   }
 
+  @ApiOperation({ summary: 'Obtener un usuario por su ID' })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.usersService.findOne(id);
   }
 
-  @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un usuario por su ID' })
+  @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(id, updateUserDto);
   }
 
+  @ApiOperation({ summary: 'Eliminar un usuario por su ID' })
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.usersService.remove(id);
   }
 }
+
+
