79dfb3da267ee9e32923cc7a59f5150f6ec363b7
Gabriel Lazo <gabriellazo48@gmail.com>
2025-08-12 20:49:38 -0300

    [FIX] clear filters

--- Frontend/src/screens/Catalog/components/FilterPanel.tsx
+++ Frontend/src/screens/Catalog/components/FilterPanel.tsx
@@ -19,6 +19,7 @@ export const FilterPanel: React.FC<FilterPanelProps> = ({
   const clearFilters = () => {
     onFiltersChange({
       categories: [],
+      brands: [],
       minPrice: "",
       maxPrice: "",
       sortBy: "name",
